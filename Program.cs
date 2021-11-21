e508391fa57046ceecfbd550c622e2639d69ea27
lodashreload <icarusjapan@gmail.com>
2021-11-21 15:36:23 -0500

    min and max set

--- Program.cs
+++ Program.cs
@@ -4,16 +4,6 @@ namespace NumberGuesser
 {
     class Program
     {
-
-        private static string Dialog(string message)
-
-        {
-            //write the nessage
-            Console.Write(message);
-            //return what the user entered 
-            return Console.ReadLine();
-        }
-
         static void DisplayGreeting()
         {
 
@@ -23,10 +13,19 @@ namespace NumberGuesser
             Console.WriteLine();
             Console.WriteLine();
         }
+        private static string Dialog(string message)
+        {
+            //write the nessage
+            Console.Write(message);
+            //return what the user entered 
+            return Console.ReadLine();
+        }
 
         static void Main(string[] args)
 
         {
+            int min = 0;
+            int max = 1024;
             //greet user (METHOD A)
             DisplayGreeting();
             //2. Prompt user to guess number between 1 and 1024
@@ -36,23 +35,19 @@ namespace NumberGuesser
             //4. If input is yes then display a RANDOMLY guessed number
             var start = Dialog("Enter yes or no:");
 
-            //4.If input is "yes" then display a RANDOMLY guessed number
+            //Changing the dialog to change name in case of "alice"
             if (start == "yes")
             {
                 Random rand = new Random();
-                int number = rand.Next(1, 1024);
+                int number = rand.Next(min, max);
                 Console.WriteLine(number);
             }
-
-            // //10. step 3: else display "Come on I don't have all day!" then return to step 3 until answer is yes //(loop?)
             else
             {
                 Console.WriteLine("Come on, I don't have all day!");
-
+                // until(Console.ReadLine == "yes");
             }
 
         }
-
     }
 }
-
